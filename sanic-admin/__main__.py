59bb98123361ffbfb8c2d085ceb5fc8cd3f5efbc
38elements <mh19820223@gmail.com>
2017-02-20 17:42:55 +0900

    Add setting

--- sanic-admin/__main__.py
+++ sanic-admin/__main__.py
@@ -1,16 +1,20 @@
 import os
 import sys
 import time
+import json
 from subprocess import Popen
 from signal import SIGTERM
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 
+SETTING_FILE_NAME = 'sanic-admin.json'
+
+
 class RestartHandler(PatternMatchingEventHandler):
 
-    def __init__(self, executable, args):
-        super().__init__(patterns=['*.py'])
+    def __init__(self, executable, args, patterns):
+        super().__init__(patterns=patterns)
         self._args = ([executable, *args])
         self._start()
         self._last_time = 0
@@ -30,9 +34,27 @@ class RestartHandler(PatternMatchingEventHandler):
         self._start()
 
 
-path = os.getcwd()
+setting_file = None
+setting = {
+    'path': None,
+    'patterns': ['*.py']
+}
+try:
+    setting_file = open(SETTING_FILE_NAME, 'r')
+except:
+    pass
+
+if setting_file:
+    _setting = json.load(setting_file)
+    setting_file.close()
+    for key in setting.keys():
+        if key in _setting:
+            setting[key] = _setting[key]
+
+
+path = setting['path'] if setting['path'] else os.getcwd()
 observer = Observer()
-handler = RestartHandler(sys.executable, sys.argv[1:])
+handler = RestartHandler(sys.executable, sys.argv[1:], setting['patterns'])
 observer.schedule(handler, path, recursive=True)
 observer.start()
 while True:
