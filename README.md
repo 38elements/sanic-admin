97d83d657d3b70e242081ba3bef4fb9ed220067d
38elements <mh19820223@gmail.com>
2017-02-24 19:14:09 +0900

    0.0.4

--- README.md
+++ README.md
@@ -26,6 +26,18 @@ You can change the behavior of `sanic-admin` by putting a file named` sanic-admi
     "patterns": ["*.html", "*.css", "*.py"],
     // Paths to be watched
     // default current working directory
-    "paths": ["/foo1/bar1", "/foo2/bar2"] 
+    "paths": ["/foo1/bar1", "/foo2/bar2"],
+    // File to be executed when sanic-admin starts
+    // default none
+    "before": "before.py",
+    // File to be executed before sanic restarts
+    // default none
+    "before_each": "before_each.py",
+    // File to be executed when sanic-admin exits
+    // default none
+    "after": "after.py",
+    // File to be executed after sanic restarts
+    // default none
+    "after_each": "after_each.py"
 }
 ```
--- sanic-admin/__main__.py
+++ sanic-admin/__main__.py
@@ -13,15 +13,21 @@ SETTING_FILE_NAME = 'sanic-admin.json'
 
 class RestartHandler(PatternMatchingEventHandler):
 
-    def __init__(self, executable, args, patterns):
+    def __init__(self, executable, args, patterns, before_each, after_each):
         super().__init__(patterns=patterns)
+        self._before_each = before_each
+        self._after_each = after_each
         self._args = ([executable, *args])
         self._start()
         self._last_time = 0
         self._skip_time = 0.75
 
     def _start(self):
+        if setting['before_each']:
+            exec(open(setting['before_each']).read())
         proc = Popen(self._args)
+        if setting['after_each']:
+            exec(open(setting['after_each']).read())
         self.pid = proc.pid
         self._last_time = time.time()
 
@@ -37,8 +43,13 @@ class RestartHandler(PatternMatchingEventHandler):
 setting_file = None
 setting = {
     'paths': [os.getcwd()],
-    'patterns': ['*.py']
+    'patterns': ['*.py'],
+    'before': None,
+    'before_each': None,
+    'after': None,
+    'after_each': None
 }
+
 try:
     setting_file = open(SETTING_FILE_NAME, 'r')
 except:
@@ -51,13 +62,19 @@ if setting_file:
         if key in _setting:
             setting[key] = _setting[key]
 
+if setting['before']:
+    exec(open(setting['before']).read())
 
-paths = setting['paths']
+handler = RestartHandler(
+    sys.executable, sys.argv[1:], setting['patterns'],
+    setting['before_each'], setting['after_each'])
+
 observer = Observer()
-handler = RestartHandler(sys.executable, sys.argv[1:], setting['patterns'])
+paths = setting['paths']
 for path in paths:
     observer.schedule(handler, path, recursive=True)
 observer.start()
+
 while True:
     try:
         time.sleep(1)
@@ -66,4 +83,6 @@ while True:
         time.sleep(0.6)
         observer.stop()
         break
+if setting['after']:
+    exec(open(setting['after']).read())
 observer.join()
